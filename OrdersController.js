6b440d16cd8c9a8f8a8e155135d305d1db0c8716
cesareduardo <cesareduardo824@gmail.com>
2018-09-04 23:29:51 -0500

    Avance, creacion de tabla con datos

--- OrdersController.js
+++ OrdersController.js
@@ -4,20 +4,71 @@ var orders =[ {
 	"products": [{
 			"product": "PIZZA",
 			"quantity": 3,
-			"price": "$15.000"
+			"price": "$10.000"
 		},
 		{
-			"product": "HAMBURGER",
+			"product": "HOTDOG",
 			"quantity": 1,
-			"price": "$12.300"
+			"price": "$1.300"
+		},{
+			"product": "COKE",
+			"quantity": 1,
+			"price": "$1.300"
+		
 		}
 			]
 		}
 	]
 	
-	
+	function genera_tabla() {
+	  // Obtener la referencia del elemento body
+	  var body = document.getElementsByTagName("body")[0];
+
+	  // Crea un elemento <table> y un elemento <tbody>
+	  var tabla   = document.createElement("table");
+	  var tblBody = document.createElement("tbody");
+	  var numOrder=orders.length -1;
+	  // Crea las celdas
+	  for (var i = 0; i < orders[numOrder].products.length; i++) {
+		// Crea las hileras de la tabla
+		var hilera = document.createElement("tr");
+
+		for (var j = 0; j < 3; j++) {
+		  // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+		  // texto sea el contenido de <td>, ubica el elemento <td> al final
+		  // de la hilera de la tabla
+		  var celda = document.createElement("td");
+		  if (j==0) {
+			  var textoCelda = document.createTextNode("Product"+orders[0].products[i].product);		  
+			  }	  
+		if (j==1) {
+			  var textoCelda = document.createTextNode("Quantity"+orders[0].products[i].quantity);		  
+			  }	  	
+			
+		if (j==2) {
+			  var textoCelda = document.createTextNode("Price"+orders[0].products[i].price);		  
+			  }	  	
+				
+			
+//		  var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+		  celda.appendChild(textoCelda);
+		  hilera.appendChild(celda);
+		}
+
+		// agrega la hilera al final de la tabla (al final del elemento tblbody)
+		tblBody.appendChild(hilera);
+	  }
+
+	  // posiciona el <tbody> debajo del elemento <table>
+	  tabla.appendChild(tblBody);
+	  // appends <table> into <body>
+	  body.appendChild(tabla);
+	  // modifica el atributo "border" de la tabla y lo fija a "2";
+	  tabla.setAttribute("border", "2");
+	}
 	function myOrder(){
 		
-		document.getElementById("Name").innerHTML=orders[0][1];
+		document.getElementById("Name").innerHTML=orders[0].products[0].price;
+		
 		
 	}
